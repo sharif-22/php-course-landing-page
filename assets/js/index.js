e2f4c9ed825566324218e36b18619a0c2379e979
kajasharif31 <kajasharif366@gmail.com>
2023-10-27 15:03:48 +0530

    feat: added dynamic styles for nav links

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,31 +1,48 @@
 // DOM Elements
 const year = document.querySelectorAll(".year");
 const hammberger = document.querySelector(".hammberger");
-const navMenu = document.querySelector(".nav-menu");
-
+const mobileNavMenu = document.querySelector("#mobileNavMenu");
 // Drop Down Menu
 function onClickHammberger() {
-  if (navMenu.classList.contains("hidden")) {
-    navMenu.classList.remove("hidden");
+  // if 🍔 change to ❌ and displaying 📱 menue
+  if (hammberger.children[0].classList.contains("bi-list")) {
     hammberger.children[0].classList.replace("bi-list", "bi-x-lg");
+
+    // changing the nav item to mobile friendly
+    mobileNavMenu.classList.remove("hidden");
+    mobileNavMenu.classList.add(
+      "absolute",
+      "top-16",
+      "bg-slate-800",
+      "h-screen",
+      "w-full",
+      "m-0",
+      "flex",
+      "flex-col",
+      "justify-center",
+      "items-center",
+      "text-white"
+      // "z-20"
+    );
   } else {
+    mobileNavMenu.classList.add("hidden");
     hammberger.children[0].classList.replace("bi-x-lg", "bi-list");
-    navMenu.classList.add("hidden");
   }
 }
 hammberger.addEventListener("click", (e) => {
   e.preventDefault();
+  collapseMobileMenu();
   onClickHammberger();
 });
 
-const navItems = navMenu.children[0].children.length;
-for (let index = 0; index < navItems; index++) {
-  // console.log(navMenu.children[0].children[index]);
-
-  navMenu.children[0].children[index].addEventListener(
-    "click",
-    onClickHammberger
-  );
+function collapseMobileMenu() {
+  // removeing mobile nav menu after every click of the menu
+  const mobileNavMenuChild = mobileNavMenu.children;
+  for (let index = 0; index < mobileNavMenuChild.length; index++) {
+    const navAnchor = mobileNavMenuChild[index].children[0];
+    // removeing mobile nav menu after user click the menu
+    navAnchor.addEventListener("click", onClickHammberger);
+  }
 }
 
 // getFullYear()
@@ -34,3 +51,27 @@ const date = new Date();
 year.forEach((value) => {
   value.textContent = date.getFullYear() + ".";
 });
+
+// dynamic nav styles
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ol li a");
+const nav = document.querySelector("nav");
+
+window.onscroll = () => {
+  sections.forEach((section) => {
+    let navHeight = nav.offsetHeight;
+    let top = window.scrollY;
+    let offset = section.offsetTop;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+
+    if (top >= offset - navHeight && top < offset - navHeight + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector(`nav ol li a[href*= "#${id}"]`)
+          .classList.add("active");
+      });
+    }
+  });
+};
